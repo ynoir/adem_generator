b04f32ecba91e636148e27a69547d319c74ae3e3
yvesn <yves.noirjean@ynoir.ch>
2019-12-19 05:10:55 +0100

    on:change

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Component from "./components/Component.svelte";
+	import Out from "./components/Out.svelte";
+	export let name;
+	const onMessage = (event) => {
+		name = event.detail.text;
+	};
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Component on:message={onMessage}/>
+	<Out text={"some text"}/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+	p {
+		color: green;
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Component.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	const onChange = (event) => {
+		dispatch("message", {
+			text: event.target.value
+		});
+	};
+
+	let count = 0;
+	$: countSquared = count**2;
+
+	let onClick = () => {
+		count += 1;
+	};
+</script>
+
+<div>
+	<p>Input:</p>
+	<input type="text" on:change={onChange}/>
+	<button on:click={onClick}>
+		{countSquared}
+	</button>
+</div>
